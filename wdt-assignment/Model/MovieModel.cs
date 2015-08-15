b4b4d4971fa2a86f8eafd892084f39cfc0703cf9
Michaelsun Baluyos <s3110401@student.rmit.edu.au>
2015-08-15 18:52:51 +1000

    Movie model has struct and creates json file. Also made test case for it

--- wdt-assignment/Model/MovieModel.cs
+++ wdt-assignment/Model/MovieModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
 using System.Collections;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,34 @@ namespace wdt_assignment.Model
 {
     class MovieModel
     {
+        private struct Movie
+        {
+            public int id;
+            public string cineplex;
+            public string title;
+            public DateTime dateTime;
+        };
+        private ArrayList movies = new ArrayList();
+        public void AddMovie(int id, string cineplex, string title, DateTime dateTime)
+        {
+            Movie movie = new Movie();
+            movie.id = id;
+            movie.cineplex = cineplex;
+            movie.title = title;
+            movie.dateTime = dateTime;
+            movies.Add(movie);
+        }
+        public void WriteJson()
+        {
+            string json = JsonConvert.SerializeObject(movies);
+            string fileName = @"db.json";
+            if (FileExists(fileName))
+                File.Delete(fileName);
+            File.WriteAllText(fileName, json);
+        }
+        public bool FileExists(String existingFilename)
+        {
+            return File.Exists(existingFilename);
+        }
     }
 }
---
+++ wdt-assignment/testc/TestMovieModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace wdt_assignment_testc
+{
+    [TestClass]
+    public class TestMovieModel
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            wdt_assignment.Model.MovieModel movie = new wdt_assignment.Model.MovieModel();
+            movie.AddMovie(0, "St Kilda", "title 1", new DateTime());
+            movie.AddMovie(1, "St1", "title 2", new DateTime());
+            movie.AddMovie(2, "St 2", "title 3", new DateTime());
+            movie.AddMovie(3, "St 3", "title 4", new DateTime());
+            movie.AddMovie(4, "St 4", "title 5", new DateTime());
+            movie.AddMovie(5, "St 5", "title 6", new DateTime());
+            movie.AddMovie(6, "St 6", "title 7", new DateTime());
+
+            movie.WriteJson();
+
+            string existingFilename = @"db.json";
+            Assert.IsTrue(movie.FileExists(existingFilename));
+            File.Delete(existingFilename);
+        }
+    }
+}
